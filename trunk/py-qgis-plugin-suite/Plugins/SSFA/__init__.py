86d70d9f814fcc63f419502f51fdbc8857faf370
Adrien.Gruson <Adrien.Gruson@55968d5a-944d-0410-a137-0700518723e3>
2008-05-19 20:05:05 +0000

    merge Plugins and tools in py-qgis-suite

---
+++ trunk/py-qgis-plugin-suite/Plugins/SSFA/__init__.py
@@ -0,0 +1,17 @@
+from show_attribut import Plugin
+
+def name():
+  return "show selection attribut"
+
+def description():
+  return "Pour voir les attributs en commun de la selection"
+
+def version():
+  return "Version 0.1"
+
+def plugin_type(): #on renvoye le bon type
+  return QgisPlugin.UI
+
+def classFactory(iface): #l'appel de la classe
+  return Plugin(iface)
+
